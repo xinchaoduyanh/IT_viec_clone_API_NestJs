087f643d6df3001f6d75928c0fe15fac483b59ad
Vu Duy Anh <duyanh19122k3@gmail.com>
2024-02-21 16:56:12 +0700

    config5

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UserController } from './user/user.controller'
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [AppController, UserController],
   providers: [AppService]
 })
