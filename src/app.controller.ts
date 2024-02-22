75a9030904c2bc327558b4698b6c90e6934b8ce5
Vu Duy Anh <duyanh19122k3@gmail.com>
2024-02-22 14:01:20 +0700

    update users dto

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
+import { ConfigService } from '@nestjs/config'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private configService: ConfigService
+  ) {}
 
   @Get()
   getHello(): string {
+    console.log(this.configService.get<string>('PORT'))
     return this.appService.getHello()
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { UserController } from './user/user.controller'
-import { UsersModule } from './users/users.module';
-
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController, UserController],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_URL')
+      }),
+      inject: [ConfigService]
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true
+    })
+  ],
+  controllers: [AppController],
   providers: [AppService]
 })
 export class AppModule {}
